9b205a3e841742721d4a562f9451a25715835337
ManvirSekhon <manvirsekhon0008@gmail.com>
2025-04-02 22:02:39 +0530

    created

---
+++ firebase/index.js
@@ -0,0 +1,36 @@
+// auth
+
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+import {auth} from "./firebase.js";
+
+
+// Main logic
+
+if(localStorage.getItem("uid") != null) {
+    window.location.href = "../html/home.html";
+}
+else {
+    document.getElementById("login").addEventListener("click", (e) => {
+        e.preventDefault();
+        login();
+    })
+    
+    
+    // functions 
+    
+    function login() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        
+        signInWithEmailAndPassword(auth, email, password)
+        .then( (userCredential) => {
+            console.log(userCredential.user.email);
+            localStorage.setItem("uid", userCredential.user.uid);
+            window.location.href = "../html/home.html";
+        })
+        .catch((error) => {alert(error.message)});
+    }
+}
