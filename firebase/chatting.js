1181038edd21b155a1c3b3f0d14c5c5e9938bcce
Manthan1418 <manthanarora2223@gmail.com>
2025-05-09 07:41:51 +0530

    finaly

--- firebase/chatting.js
+++ firebase/chatting.js
@@ -24,6 +24,7 @@ if (!groupName) {
 }
 
 document.getElementById("chatGroup-name-header").innerText = groupName;
+document.getElementById("userName-display").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg> ${localStorage.getItem("username")}`;
 const messageInput = document.getElementById('messageInput');
 const messageBtn = document.getElementById("sendMessageBtn");
 const userName = localStorage.getItem("username");
